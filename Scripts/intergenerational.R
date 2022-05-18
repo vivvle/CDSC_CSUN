a041d6918333748513c70d175d8c47e9f815cebe
vivvle <vivianvnle@gmail.com>
2022-05-17 17:20:04 -0700

    uploading intergenerational script and outputs

---
+++ Scripts/intergenerational.R
@@ -0,0 +1,147 @@
+#### Intergenerational Conversation Webinar ####
+#### Created by: Vivian Vy Le ####
+#### Updated on: 2022-05-17 ####
+
+#### Load library ####
+library(tidyverse)
+library(here)
+library(ggplot2)
+
+
+#### Load data ####
+intergen <- read_csv(here("Data", "intergenerational_convo.csv"))
+
+
+#### View data ####
+view(intergen)
+
+
+#### Data analysis ####
+intergen_clean <- intergen %>%
+  select(participants, overall_rating, speaker_rating, advertisement, event_timing, audience_learning) %>%
+  rowwise() %>%
+  mutate(advertisement_choices = str_split(advertisement, pattern = ";")) %>%
+  ungroup() %>%
+  unnest(advertisement_choices)
+view(intergen_clean)
+
+advertisement_data <- intergen_clean %>%
+  count(advertisement_choices)
+view(advertisement_data)
+
+data <- intergen_clean %>%
+  mutate(overall_rating = factor(overall_rating,
+                               levels = c("Excellent", "Fairly Good", "Not Good at All"))) %>%
+  mutate(speaker_rating = factor(speaker_rating,
+                                 levels = c("Excellent", "Fairly Good", "Not Good at All"))) %>%
+  mutate(event_timing = factor(event_timing,
+                               levels = c("Extremely convenient", "Somewhat convenient", "Not at all convenient"))) %>%
+  mutate(audience_learning = factor(audience_learning,
+                                    levels = c("Very much", "Somewhat", "Not much")))
+view(data)
+
+overall_rating <- data %>%
+  count(overall_rating)
+
+speaker_rating <- data %>%
+  count(speaker_rating)
+
+event_timing <- data %>%
+  count(event_timing)
+
+audience_data <- data %>%
+  count(audience_learning)
+
+
+#### plot ####
+advertisement_data %>%
+  ggplot(aes(x = reorder(advertisement_choices, -n), y = n, fill = advertisement_choices)) +
+  geom_col() +
+  labs(title = "How did respondants learn from the webinar on intergenerational conversation on activism?",
+       x = "Responses",
+       y = "Counts") +
+  theme(plot.title = element_text(hjust = 0.5,
+                                  size = 10,
+                                  face = "bold"),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(color = "#000000",
+                                   angle = 90),
+        axis.text.y = element_text(color = "#000000"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) +
+  scale_fill_viridis_d() +
+  guides(fill = FALSE)
+ggsave(here("Output", "intergenerational", "advertisement.png"), width = 8, height = 5)
+
+
+overall_rating %>%
+  ggplot(aes(x = reorder(overall_rating, -n), y = n, fill = overall_rating)) +
+  geom_col() +
+  labs(title = "How did respondants rate the event overall?",
+       x = "Responses",
+       y = "Counts") +
+  theme(plot.title = element_text(hjust = 0.5,
+                                  size = 10,
+                                  face = "bold"),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(color = "#000000"),
+        axis.text.y = element_text(color = "#000000"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) +
+  scale_fill_viridis_d() +
+  guides(fill = FALSE)
+ggsave(here("Output", "intergenerational", "event_rating.png"), width = 6, height = 5)
+
+speaker_rating %>%
+  ggplot(aes(x = reorder(speaker_rating, -n), y = n, fill = speaker_rating)) +
+  geom_col() +
+  labs(title = "How did respondants rate the speakers?",
+       x = "Responses",
+       y = "Counts") +
+  theme(plot.title = element_text(hjust = 0.5,
+                                  size = 10,
+                                  face = "bold"),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(color = "#000000"),
+        axis.text.y = element_text(color = "#000000"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) +
+  scale_fill_viridis_d() +
+  guides(fill = FALSE)
+ggsave(here("Output", "intergenerational", "speaker_rating.png"), width = 6, height = 5)
+
+event_timing %>%
+  ggplot(aes(x = reorder(event_timing, -n), y = n, fill = event_timing)) +
+  geom_col() +
+  labs(title = "How convenient was the event for the respondents?",
+       x = "Responses",
+       y = "Counts") +
+  theme(plot.title = element_text(hjust = 0.5,
+                                  size = 10,
+                                  face = "bold"),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(color = "#000000"),
+        axis.text.y = element_text(color = "#000000"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) +
+  scale_fill_viridis_d() +
+  guides(fill = FALSE)
+ggsave(here("Output", "intergenerational", "convenience_of_event.png"), width = 6, height = 5)
+
+learning_data %>%
+  ggplot(aes(x = reorder(audience_learning, -n), y = n, fill = audience_learning)) +
+  geom_col() +
+  labs(title = "Did the respondents learn from the event?",
+       x = "Responses",
+       y = "Counts") +
+  theme(plot.title = element_text(hjust = 0.5,
+                                  size = 10,
+                                  face = "bold"),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(color = "#000000"),
+        axis.text.y = element_text(color = "#000000"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) +
+  scale_fill_viridis_d() +
+  guides(fill = FALSE)
+ggsave(here("Output", "intergenerational", "audience_learning.png"), width = 6, height = 5)
